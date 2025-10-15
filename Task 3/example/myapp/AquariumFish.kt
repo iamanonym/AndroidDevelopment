59bfdf2866e7cb2578c38d07ead722b62505b232
iamanonym <s.masalsky@yandex.ru>
2025-10-15 10:15:36 +0500

    Task 3

---
+++ Task 3/example/myapp/AquariumFish.kt
@@ -0,0 +1,54 @@
+package example.myapp
+
+abstract class AquariumFish : FishAction {
+    abstract val color: String
+    override fun eat() = println("yum")
+}
+
+class Shark: AquariumFish(), FishAction {
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus: AquariumFish(), FishAction {
+    override val color = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
+}
+
+interface FishAction  {
+    fun eat()
+}
+
+interface FishColor {
+    val color: String
+}
+
+class Plecostomus2(fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+{
+    override fun eat() {
+        println("eat algae")
+    }
+}
+
+class Shark2: FishAction, FishColor {
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
